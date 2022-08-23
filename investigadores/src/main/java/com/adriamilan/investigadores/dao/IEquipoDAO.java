a7ce94d7275bde3776bd52cca09373ffb4ae79ac
Adrià Milan <adria4milan@gmail.com>
2022-08-23 23:27:47 +0200

    Deploy Heroku

--- investigadores/src/main/java/com/adriamilan/investigadores/dao/IEquipoDAO.java
+++
@@ -1,9 +0,0 @@
-package com.adriamilan.investigadores.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.adriamilan.investigadores.dto.Equipo;
-
-public interface IEquipoDAO extends JpaRepository<Equipo, Long> {
-
-}
--- investigadores/src/main/java/com/adriamilan/investigadores/dao/IInvestigadorDAO.java
+++
@@ -1,9 +0,0 @@
-package com.adriamilan.investigadores.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.adriamilan.investigadores.dto.Investigador;
-
-public interface IInvestigadorDAO extends JpaRepository<Investigador, Long> {
-
-}
